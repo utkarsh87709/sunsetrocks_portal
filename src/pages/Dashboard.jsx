e125b9273ea62e7889a5d492d8939e58f01777bb
rishi231298 <rishisingh2322@gmail.com>
2025-09-03 10:04:48 +0530

    changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -164,7 +164,7 @@ const Dashboard = () => {
             </div>
           </div>
 
-          {/* Stats Cards */}
+     
           <div className="lg:col-span-2">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
               {stats.map((stat) => (
